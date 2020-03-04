d8689b25a2c90ee0f5af9a8ab73f9a3b28057dde
Sergey Borisevich <borisevich.serjyk@gmail.com>
2020-03-04 13:38:30 +0300

    feat: brackets is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) return false;
+    let strArr = str.split('');
+    let completes = bracketsConfig.map((arr) => arr.join(''));
+
+    for (let i = 0; i < str.length; i++) {
+        for (let x = 0; x < completes.length; x++) {
+            if (strArr[i] === completes[x][0] && strArr[i + 1] === completes[x][1]) {
+                strArr.splice(i, 2);
+                i = -1;
+            }
+        }
+    }
+    return strArr.length == 0 ? true : false;
 }
